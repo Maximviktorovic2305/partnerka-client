4de02b0cf65cd3de9c9ec654b9e2f0685b19d8e3
Maximviktorovic2305 <Maximviktorovic123@ya.ru>
2024-12-09 12:55:30 +1000

    asdasd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import HomeComponent from "@/components/HomeComponent";
+import { Suspense } from "react";
 
 export default function Home() {
 	return (
 		<div className='size-full min-h-screen text-center text-lg mt-20'>
 			<div>Добро пожаловать на тестовый сайт Partners</div>
-			<HomeComponent />
+			<Suspense fallback={<div>Загрузка...</div>}>
+        		<HomeComponent />
+    		</Suspense>
 		</div>
 	)
 }
